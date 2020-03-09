72855d76dffd6d4ee6b876d2c87069e48803fffd
Sven Rahn <apps.randombyte@gmail.com>
2020-03-09 16:20:23 +0100

    Added possibility to specify more than one top level folder, added more audio file formats, fixed that the top level folder was also crated as a m3u file

--- src/main/kotlin/de/randombyte/crates/CratesGenerator.kt
+++ src/main/kotlin/de/randombyte/crates/CratesGenerator.kt
@@ -3,34 +3,46 @@ package de.randombyte.crates
 import java.nio.file.Paths;
 import java.io.BufferedWriter;
 import java.io.File;
+import kotlin.system.exitProcess
+
+val AUDIO_FILE_EXTENSIONS = listOf("mp3", "m4a", "opus")
 
 fun BufferedWriter.writeLine(line: String) {
   write(line)
   newLine()
 }
 
-fun getFilesRecursively(folder: File): List<String> {
-  return folder.walk().filter { it.isFile && it.extension == "mp3" }.map { it.absolutePath }.toList()
+fun getAudioFilesRecursively(folder: File): List<String> {
+  return folder.walk().filter { it.isFile && it.extension in AUDIO_FILE_EXTENSIONS }.map { it.absolutePath }.toList()
 }
 
 fun main(args: Array<String>) {
 
-  if (args.size != 2) {
-    println("Usage: './generateCrates.kts <foldersPath> <crateFilesDestination>'")
-    System.exit(0)
+  if (args.size < 2) {
+    println("Arguments: '<folderPaths...> <crateFilesDestination>'")
+    exitProcess(0)
   }
-  
-  val path = Paths.get(args[0])
-  val crateTrackPaths = path.toFile().walk()
-	  .filter { file -> file.isDirectory() }.map { dir -> dir.name to getFilesRecursively(dir) }
-  
-  crateTrackPaths.forEach { (crateName, trackPaths) ->
-    Paths.get(args[1], crateName + ".m3u").toFile().apply { parentFile.mkdirs(); createNewFile() }.bufferedWriter().use { writer ->
-      writer.writeLine("#EXTM3U")
 
-      trackPaths.forEach { trackPath ->
-        writer.writeLine("#EXTINF")
-        writer.writeLine(trackPath)
+  val folderPaths = args.dropLast(1)
+  val crateFilesDestinationPath = Paths.get(args.last())
+
+  folderPaths.forEach { folderStringPath ->
+    val folderPath = Paths.get(folderStringPath)
+    val crateTrackPaths = folderPath.toFile()
+      .walk()
+      .filter { file -> file.isDirectory && file.toPath() != folderPath } // prevent writing down the top level folder
+      .map { dir -> dir.name to getAudioFilesRecursively(dir) }
+
+    crateTrackPaths.forEach { (crateName, trackPaths) ->
+      crateFilesDestinationPath.resolve("$crateName.m3u")
+        .toFile()
+        .apply { parentFile.mkdirs(); createNewFile() }
+        .bufferedWriter().use { writer ->
+          writer.writeLine("#EXTM3U")
+          trackPaths.forEach { trackPath ->
+            writer.writeLine("#EXTINF")
+            writer.writeLine(trackPath)
+          }
       }
     }
   }
